6219f1736d7ffa0e6a3d809ddacb6ea003ff2f66
Eduardo Preciado <hi@ejpreciado.com>
2022-01-12 17:21:50 -0500

    Remove unnecessary path additions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,10 @@
 set fish_greeting
 set -gx EDITOR nvim
 
-fish_add_path "/opt/homebrew/bin"
+# homebrew
+fish_add_path "/usr/local/sbin"
 
+# volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
