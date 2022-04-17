d166d41e4df610cd9d32aa3ca38f05c652a21bd2
Eduardo Preciado <hi@ejpreciado.com>
2022-04-17 14:41:08 -0400

    Add fish function back in

---
+++ fish/.config/fish/functions/gbp.fish
@@ -0,0 +1,3 @@
+function gbp --description 'List git branches by author date'
+  git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'
+end
