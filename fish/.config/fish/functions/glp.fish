ddadd7aab5682b5427e6c9f84184e7e996e37e1b
Eduardo Preciado <hi@ejpreciado.com>
2022-04-17 15:50:14 -0400

    Update formatting on glp fish function

--- fish/.config/fish/functions/glp.fish
+++ fish/.config/fish/functions/glp.fish
@@ -1,3 +1,3 @@
 function glp --description 'alias glp git log --pretty'
-  git log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short
+  git log --pretty=format:"%C(yellow)%h %C(magenta)%ad%C(red)%d %Creset%s%C(blue) [%cn]" --decorate --date=short
 end
