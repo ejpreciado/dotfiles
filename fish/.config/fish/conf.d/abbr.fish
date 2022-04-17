63cf47b9df3ec09e98d256f1dbe1ff2eb0b48c00
Eduardo Preciado <hi@ejpreciado.com>
2022-04-17 15:30:21 -0400

    Add more fish abbreviations

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -1,28 +1,43 @@
 # shell
 
 # git
+abbr --add g    'git'
 abbr --add ga   'git add'
 abbr --add gap  'git add -p'
+
 abbr --add gb   'git branch'
+
 abbr --add gc   'git commit --verbose'
 abbr --add gca  'git commit -a --verbose'
 abbr --add gcam 'git commit -am'
 abbr --add gcm  'git commit -m'
+abbr --add gm   'git commit --amend --verbose'
+
 abbr --add gco  'git checkout'
+
 abbr --add gd   'git diff'
 abbr --add gdc  'git diff --cached'
 abbr --add gdd  'git diff --color-words'
 abbr --add gddc 'git diff --color-words --cached'
 abbr --add gds  'git diff --stat'
 abbr --add gdsc 'git diff --stat --cached'
+
 abbr --add gl   'git log --graph --all'
-abbr --add gm   'git commit --amend --verbose'
+
 abbr --add gp   'git pull'
 abbr --add gpp  'git pull; and git push --force-with-lease'
 abbr --add gpu  'git push --force-with-lease'
+
 abbr --add grbi 'git rebase -i'
 abbr --add grbc 'git rebase --continue'
 abbr --add grba 'git rebase --abort'
+
+abbr --add gsta 'git stash'
+abbr --add gstv 'git stash save'
+abbr --add gsts 'git stash show --text'
+abbr --add gstp 'git stash pop'
+abbr --add gstd 'git stash drop'
+
 abbr --add gs   'git status --short'
 abbr --add gsw  'git switch'
 abbr --add gsw- 'git switch -'
