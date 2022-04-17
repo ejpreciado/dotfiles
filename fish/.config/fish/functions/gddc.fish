66a3f7185fc338c188b1c9ca995769c4931f4628
Eduardo Preciado <hi@ejpreciado.com>
2022-04-17 00:46:30 -0400

    Rename fish function to use different flag

---
+++ fish/.config/fish/functions/gddc.fish
@@ -0,0 +1,3 @@
+function gddc --description 'alias gdds git diff --color-words --cached'
+  git diff --color-words --cached $argv;
+end
--- fish/.config/fish/functions/gdds.fish
+++
@@ -1,3 +0,0 @@
-function gdds --description 'alias gdds git diff --color-words --staged'
-  git diff --color-words --staged $argv;
-end
