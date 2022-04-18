94a42e0038f4d2eb32c78c6dd596b759c14b78f7
Eduardo Preciado <hi@ejpreciado.com>
2022-04-17 20:30:37 -0400

    Update current_branch function

--- fish/.config/fish/functions/current_branch.fish
+++ fish/.config/fish/functions/current_branch.fish
@@ -1,5 +1,3 @@
 function current_branch --description 'prints current git branch name'
-    set ref (git symbolic-ref HEAD 2> /dev/null); or \
-    set ref (git rev-parse --short HEAD 2> /dev/null); or return
-    echo $ref | sed -e 's|^refs/heads/||'
+  git symbolic-ref --short HEAD
 end
