8f4feb974606c23b54e554091cc84bdb67ae4276
Eduardo Preciado <hi@ejpreciado.com>
2022-04-23 16:31:12 -0400

    Add pyenv setup to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -24,6 +24,14 @@ if test -d $HOME/.cargo/bin
   fish_add_path "$HOME/.cargo/bin"
 end
 
+# pyenv
+if test -d $HOME/.pyenv
+  set -gx PYENV_VIRTUALENV_DISABLE_PROMPT 1
+  status is-login; and pyenv init --path | source
+  status is-interactive; and pyenv init - | source
+  status is-interactive; and pyenv virtualenv-init - | source
+end
+
 # starship
 if type -q starship
   starship init fish | source
