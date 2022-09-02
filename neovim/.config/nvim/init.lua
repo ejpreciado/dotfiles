2bb5fb8e4c39df1dcbf3858814de9ea1dc4047cf
Eduardo Preciado <hi@ejpreciado.com>
2022-09-02 10:16:28 -0400

    Move surround plugin settings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -80,6 +80,19 @@ require"telescope".setup{
 
 require('telescope').load_extension('fzf')
 
+-- surround
+require'surround'.setup {
+  context_offset = 100,
+  load_autogroups = false,
+  mappings_style = 'sandwich',
+  map_insert_mode = true,
+  quotes = {"'", '"'},
+  brackets = {'(', '{', '['},
+  space_on_closing_char = false,
+  prefix = 's',
+}
+
+
 -- bufferline
 require("bufferline").setup{}
 
