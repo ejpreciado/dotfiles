3ef3676a24fcb3fa6d4d3bd09e114d39e9568aff
Eduardo Preciado <hi@ejpreciado.com>
2022-11-25 03:56:04 -0500

    Update LSP configs

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -57,17 +57,17 @@ require("mason-lspconfig").setup({
     ensure_installed = { "sumneko_lua", "ruby_ls" }
 })
 
--- lsp
-require("lspconfig").sumneko_lua.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" }
-      }
-    }
-  }
+-- lsp 
+require("mason-lspconfig").setup_handlers({
+  function(server_name)
+    require("lspconfig")[server_name].setup({})
+  end,
+  ["sumneko_lua"] = function ()
+    require("lspconfig").sumneko_lua.setup({
+      settings={Lua={diagnostics={globals={"vim"}}}}
+    })
+  end
 })
-require("lspconfig").ruby_ls.setup({})
 
 -- gitsigns
 require("gitsigns").setup()
